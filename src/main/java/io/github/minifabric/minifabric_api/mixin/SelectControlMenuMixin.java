c4414fe45b5d41c8f3ab149ee57da397b2d98022
PseudoDistant <javafarlander2002@gmail.com>
2023-12-28 07:19:15 -0500

    Fix softlock when actually *using* Delux Mod Menu

---
+++ src/main/java/io/github/minifabric/minifabric_api/mixin/SelectControlMenuMixin.java
@@ -0,0 +1,24 @@
+package io.github.minifabric.minifabric_api.mixin;
+
+import com.mojang.ld22.screen.controlmenu.ControlMenu;
+import com.mojang.ld22.screen.controlmenu.SelectControlMenu;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(SelectControlMenu.class)
+public class SelectControlMenuMixin extends ControlMenu {
+	@Shadow protected int selected;
+	public SelectControlMenuMixin(ControlMenu parent) {
+		super(parent);
+	}
+	@Redirect(method = "tick", at = @At(value = "INVOKE", target = "Lcom/mojang/ld22/screen/controlmenu/SelectControlMenu;optionSelected(I)V"))
+	public void removeClose(SelectControlMenu instance, int i) {
+		if (this.input.attack.clicked || this.input.menu.clicked) {
+			((SelectControlMenu)(Object)this).optionSelected(selected);
+		} else if (this.input.close.clicked) {
+			this.gameControl.setMenu(this.parent);
+		}
+	}
+}
